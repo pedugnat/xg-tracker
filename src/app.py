e4deeaeee0d59348bcfa8aedc78a4b9cf0c131e7
Paul-Emile Dugnat <pedugnat@gmail.com>
2021-02-26 13:14:02 +0100

    add other countries

--- src/app.py
+++ src/app.py
@@ -8,23 +8,17 @@ st.set_page_config(page_title="xG Tracker",
                    layout='wide',
                    initial_sidebar_state='auto')
 
-st.markdown(
-    f'''
-        <style>
-            .sidebar .sidebar-content {{
-                width: 375px;
-            }}
-        </style>
-    ''',
-    unsafe_allow_html=True
-)
-
 st.title("xG Tracker : quels joueurs surperforment ?")
 st.text("")
 
+country_choice = st.sidebar.selectbox(
+    'Quelle pays veux-tu analyser ?',
+    config.LIST_OF_COUNTRIES,
+    index=0)
+
 team_choice = st.sidebar.selectbox(
     'Quelle équipe veux-tu analyser ?',
-    config.LIST_OF_TEAMS,
+    config.COUNTRY_TEAMS[country_choice],
     index=0)
 
 year_choice = st.sidebar.selectbox(
@@ -40,7 +34,7 @@ goal_options = st.sidebar.checkbox("Montrer les buts", value=True)
 st.sidebar.text("")
 assist_options = st.sidebar.checkbox("Montrer les assists", value=True)
 
-if team_choice != "<Choix d'une équipe>":
+if (team_choice != "<Choix d'une équipe>") & (country_choice != "<Choix d'un pays>"):
     html_team_table = get_xG_html_table(team_choice, year=year_choice)
     df_team = process_html(html_team_table)
 
--- src/config.py
+++ src/config.py
@@ -1,11 +1,45 @@
-CHROMEDRIVER_PATH = "chromedriver_upd"
+CHROMEDRIVER_PATH = "src/chromedriver_upd"
 CACHE_PATH = "data_cache"
-
-LIST_OF_TEAMS = ["<Choix d'une équipe>", "Lille", "Lyon", "Paris_Saint_Germain",
-                 "Monaco", "Lens", "Metz", "Marseille",
-                 "Rennes", "Lorient", "Strasbourg",
-                 "Montpellier", "Bordeaux", "Nice", "Brest",
-                 "Angers", "Nantes", "Reims", "Nantes",
-                 "Dijon", "Nimes"]
-
 LIST_OF_YEARS = [2020, 2019, 2018, 2017, 2016, 2015, 2014]
+
+LIST_OF_COUNTRIES = ["<Choix d'un pays>", "France", "Espagne",
+                     "Angleterre", "Allemagne", "Italie"]
+
+CHOIX_TEAMS = ["<Choix d'un pays>"]
+
+FRENCH_TEAMS = ["<Choix d'une équipe>", "Lille", "Lyon", "Paris_Saint_Germain",
+                "Monaco", "Lens", "Metz", "Marseille",
+                "Rennes", "Lorient", "Strasbourg",
+                "Montpellier", "Bordeaux", "Nice", "Brest",
+                "Angers", "Nantes", "Reims", "Nantes",
+                "Dijon", "Nimes"]
+
+GERMAN_TEAMS = ["<Choix d'une équipe>", 'Werder_Bremen', 'VfB_Stuttgart', 'Union_Berlin', 'Schalke_04',
+                'RasenBallsport_Leipzig', 'Mainz_05', 'Hoffenheim',
+                'Hertha_Berlin', 'Freiburg', 'FC_Cologne', 'Eintracht_Frankfurt',
+                'Borussia_M.Gladbach', 'Borussia_Dortmund', 'Bayern_Munich',
+                'Bayer_Leverkusen', 'Augsburg', 'Arminia_Bielefeld']
+
+ENGLISH_TEAMS = ["<Choix d'une équipe>", 'Manchester_City', 'Manchester_United', 'Leicester', 'West_Ham', 'Chelsea',
+                 'Liverpool', 'Everton', 'Aston_Villa', 'Tottenham', 'Leeds',
+                 'Arsenal', 'Wolverhampton_Wanderers', 'Crystal_Palace',
+                 'Southampton', 'Burnley', 'Brighton', 'Newcastle_United', 'Fulham',
+                 'West_Bromwich_Albion', 'Sheffield_United']
+
+SPANISH_TEAMS = ["<Choix d'une équipe>", 'Atletico_Madrid', 'Real_Madrid', 'Barcelona', 'Sevilla',
+                 'Real_Sociedad', 'Villarreal', 'Real_Betis', 'Levante', 'Granada',
+                 'Athletic_Club', 'Celta_Vigo', 'Valencia', 'Osasuna', 'Cadiz',
+                 'Getafe', 'Alaves', 'Eibar', 'Real_Valladolid', 'Elche',
+                 'SD_Huesca']
+
+ITALIAN_TEAMS = ["<Choix d'une équipe>", 'Inter', 'AC_Milan', 'Juventus', 'Roma', 'Atalanta', 'Lazio',
+                 'Napoli', 'Sassuolo', 'Verona', 'Sampdoria', 'Genoa', 'Bologna',
+                 'Udinese', 'Fiorentina', 'Benevento', 'Spezia', 'Torino',
+                 'Cagliari', 'Parma_Calcio_1913', 'Crotone']
+
+COUNTRY_TEAMS = {"France": FRENCH_TEAMS,
+                 "Espagne": SPANISH_TEAMS,
+                 "Angleterre": ENGLISH_TEAMS,
+                 "Allemagne": GERMAN_TEAMS,
+                 "Italie": ITALIAN_TEAMS,
+                 "<Choix d'un pays>": CHOIX_TEAMS}
