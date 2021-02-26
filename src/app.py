dd328a3ee5c14864b085ac7cc90d4abbd2d36d89
Paul-Emile Dugnat <pedugnat@gmail.com>
2021-02-26 21:53:50 +0100

    minor modifs

--- src/app.py
+++ src/app.py
@@ -5,6 +5,44 @@ import config
 from utils import (get_xG_html_table, make_situation_chart, plot_xG_df,
                    process_html, make_quality_shot_chart)
 
+
+def make_sidebar():
+    st.sidebar.header("Paramètres")
+
+    country_choice = st.sidebar.selectbox(
+        'Quelle pays veux-tu analyser ?',
+        config.LIST_OF_COUNTRIES,
+        index=0)
+
+    team_choice = st.sidebar.selectbox(
+        'Quelle équipe veux-tu analyser ?',
+        config.COUNTRY_TEAMS[country_choice],
+        index=0)
+
+    year_choice = st.sidebar.selectbox(
+        'Quelle année veux-tu analyser ?',
+        config.LIST_OF_YEARS,
+        index=0)
+
+    st.sidebar.header("Analyses")
+    st.sidebar.subheader("Par joueur")
+
+    goal_options = st.sidebar.checkbox("Montrer les buts", value=True)
+    assist_options = st.sidebar.checkbox("Montrer les assists", value=True)
+
+    st.sidebar.subheader("Par équipe")
+
+    situations_options = st.sidebar.checkbox(
+        "Montrer les situations", value=True)
+    shots_quality_options = st.sidebar.checkbox(
+        "Montrer la qualité des tirs", value=True)
+
+    parameters = country_choice, team_choice, year_choice
+    analysis = goal_options, assist_options, situations_options, shots_quality_options
+
+    return parameters, analysis
+
+
 st.set_page_config(page_title="xG Tracker",
                    layout='wide',
                    initial_sidebar_state='auto')
@@ -13,37 +51,11 @@ st.title("xG Tracker")
 st.subheader("Quels équipes / joueurs surperforment ?")
 st.text("")
 
-st.sidebar.header("Paramètres")
+parameters, analysis = make_sidebar()
 
-country_choice = st.sidebar.selectbox(
-    'Quelle pays veux-tu analyser ?',
-    config.LIST_OF_COUNTRIES,
-    index=0)
+country_choice, team_choice, year_choice = parameters
+goal_options, assist_options, situations_options, shots_quality_options = analysis
 
-team_choice = st.sidebar.selectbox(
-    'Quelle équipe veux-tu analyser ?',
-    config.COUNTRY_TEAMS[country_choice],
-    index=0)
-
-year_choice = st.sidebar.selectbox(
-    'Quelle année veux-tu analyser ?',
-    config.LIST_OF_YEARS,
-    index=0)
-
-st.sidebar.header("Analyses")
-
-st.sidebar.text("")
-goal_options = st.sidebar.checkbox("Montrer les buts", value=True)
-
-st.sidebar.text("")
-assist_options = st.sidebar.checkbox("Montrer les assists", value=True)
-
-st.sidebar.text("")
-situations_options = st.sidebar.checkbox("Montrer les situations", value=True)
-
-st.sidebar.text("")
-shots_quality_options = st.sidebar.checkbox(
-    "Montrer la qualité des tirs", value=True)
 
 if (team_choice != "<Choix d'une équipe>") & (country_choice != "<Choix d'un pays>"):
     html_team_table = get_xG_html_table(team_choice, year=year_choice)
@@ -79,4 +91,13 @@ if (team_choice != "<Choix d'une équipe>") & (country_choice != "<Choix d'un pa
         st.header("Qualité des tirs pour et contre")
         st.bokeh_chart(quality_shot_char)
 
-    st.info('Source: https://understat.com/league/Ligue_1')
+    st.info('Source: https://understat.com/')
+
+# hide footer "Made with Streamlit"
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
