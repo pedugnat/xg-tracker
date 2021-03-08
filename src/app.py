66fa3faed191ee0546c2ab8ef2e24ebc3cd2d67b
Paul-Emile Dugnat <pedugnat@gmail.com>
2021-03-08 19:39:05 +0100

    added description on each graph

--- src/app.py
+++ src/app.py
@@ -44,10 +44,44 @@ if (team_choice != "<Choix d'une équipe>") & (country_choice != "<Choix d'un pa
 
     if goal_options:
         st.header("Goals vs xGoals")
+
+        meaning_xg_graph = st.checkbox(
+            "Que représente ce graph ?", key="goals_graph")
+        if meaning_xg_graph:
+            st.markdown("**Les xGoals de chaque joueur sont indiqués en abscisse (axe horizontal). "
+                        "Les buts effectivement marqués sont indiqués en "
+                        "ordonnée (axe vertical). Chaque point représente un joueur de "
+                        "l'équipe, et la couleur représente l'écart entre les "
+                        "xGoals et les buts marqués. Plus un point est vert, "
+                        "plus le joueur est efficace devant le but ; à "
+                        "l'inverse, plus un point est rouge, plus un joueur a "
+                        "raté des occasions de marquer. Ainsi, un joueur normal "
+                        "devrait marquer autant de but réels que de xGoals "
+                        "(ligne noire). Un attaquant de grande classe devrait "
+                        "se situer au dessus (zone de surperformance), tandis "
+                        "qu'un attaquant moyen se situera en dessous (zone de "
+                        "sous-performance). **")
+
         st.bokeh_chart(goal_plot)
 
     if assist_options:
         st.header("Assists vs xAssists")
+
+        meaning_xa_graph = st.checkbox(
+            "Que représente ce graph ?", key="assists_graph")
+        if meaning_xa_graph:
+            st.markdown("**Les xAssists de chaque joueur sont indiqués en abscisse (axe horizontal). "
+                        "Les passes dé (assists) effectivement données sont indiqués en "
+                        "ordonnée (axe vertical). Chaque point représente un joueur de "
+                        "l'équipe, et la couleur représente l'écart entre les "
+                        "xAssists et les passe dés réellement données. Ainsi, "
+                        "un joueur normal devrait donner autant de passes dé "
+                        "qu'il n'a d'xAssists (ligne noire). Un joueur "
+                        "dont les passes dé se transforment beaucoup en buts devrait "
+                        "se situer au-dessus (zone de surperformance), tandis "
+                        "qu'un passeur dont les passes ne sont pas converties se "
+                        "situera au-dessous (zone de sous-performance). **")
+
         st.bokeh_chart(assist_plot)
 
     if top_players_options:
@@ -89,10 +123,33 @@ if (team_choice != "<Choix d'une équipe>") & (country_choice != "<Choix d'un pa
 
     if situations_options:
         st.header("Différence xG par situation de jeu")
+
+        meaning_diff_situations = st.checkbox(
+            "Que représente ce graph ?", key="situations_graph")
+        if meaning_diff_situations:
+            st.markdown("**Ce graph représente la différence entre le nombre de "
+                        "buts marqués et le nombre de xGoals par situation de jeu. Cela "
+                        "permet d'idenfitier les situations dans lesquelles une équipe "
+                        "sur ou sous-performe sur la saison, et donc ses points "
+                        "forts ou faibles. **")
+
         st.bokeh_chart(situation_chart)
 
     if shots_quality_options:
         st.header("Qualité des tirs pour et contre")
+
+        meaning_diff_shots = st.checkbox(
+            "Que représente ce graph ?", key="differentiel_graph")
+        if meaning_diff_shots:
+            st.markdown("**Ce graph représente la différence de qualité de tirs "
+                        "entre les tirs proposés et encaissés par une équipe. "
+                        "Plus une équipe proposera des tirs de qualité "
+                        "plus la moyenne xG par tir sera élevée. Plus une équipe"
+                        "concèdera des tirs de qualité, plus sa moyenne xGA par "
+                        "tir sera élevée, signe d'une vulnérabilité. Le différentiel "
+                        "est donc un outil pour évaluer la domination d'une "
+                        "équipe en fonction des situations de jeu. **")
+
         st.bokeh_chart(quality_shot_char)
 
     st.text("")
